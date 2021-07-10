083b0cb58757f5986f3030005380275b1749895d
Talha Naeem <50256402+Talha-Naeem@users.noreply.github.com>
2021-07-10 22:49:20 +0500

    MVC PAGES
    MVC PAGES

---
+++ ASP .NET MVC PAGES/Controllers/TeacherController.cs
@@ -0,0 +1,136 @@
+﻿using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCapp.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private FirstMVCdbEntities db = new Models.FirstMVCdbEntities();
+        // GET: Teacher
+        public ActionResult Index()
+        {
+            return View(db.Teachers.ToList());
+        }
+
+        // GET: Teacher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // GET: Teacher/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Teacher/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Teacher teacher)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    db.Teachers.Add(teacher);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(teacher);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Teacher/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Teacher teacher)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    db.Entry(teacher).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(teacher);
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Teacher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: Teacher/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Teacher teacher = db.Teachers.Find(id);
+                db.Teachers.Remove(teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
